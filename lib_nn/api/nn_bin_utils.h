55eaf781fcc2a5bf752d847923b33af2b33c509c
Keith Moulton <keithm@xmos.com>
2021-02-02 17:15:08 -0500

    updated copyright notice in c, xc and asm sources

--- lib_nn/api/nn_bin_utils.h
+++ lib_nn/api/nn_bin_utils.h
@@ -1,3 +1,5 @@
+// Copyright 2021 XMOS LIMITED. This Software is subject to the terms of the 
+// XMOS Public License: Version 1
 #ifndef NN_BNN_UTILS_H
 #define NN_BNN_UTILS_H
 
--- lib_nn/src/c/util/binary/bin_utils.c
+++ lib_nn/src/c/util/binary/bin_utils.c
@@ -1,3 +1,5 @@
+// Copyright 2021 XMOS LIMITED. This Software is subject to the terms of the 
+// XMOS Public License: Version 1
 #include "nn_bin_utils.h"
 
 void set_bit_b32(bnn_b32_t* x, unsigned ch, bnn_bool_t val) {
