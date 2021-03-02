d4be3ccb389d5fcd9510e2208cce393e3f068691
Andrew Stanford-Jason <andrew@xmos.com>
2021-03-02 15:49:21 +0000

    vpu memcpy test clean up

--- test/unit_test/src/test_vpu_memcpy.c
+++ test/unit_test/src/test_vpu_memcpy.c
@@ -31,12 +31,16 @@ void test_vpu_memcpy_directed_0(){
     TEST_ASSERT_EQUAL_INT8_ARRAY(dst, src, DIR_TEST_0_BYTES);
 }
 
-void test_vpu_memcpy_pseudo_random(){
 
-    const size_t bytes_per_vpu_word = 32;
-    const size_t max_test_vpu_words = 24;
+void impl_vpu_memcpy_pseudo_random(
+    size_t pointer_inc, 
+    size_t cpy_bytes_inc,
+    int max_test_vpu_words ){
+
+    const size_t bytes_per_vpu_word = XS3_VPU_VREG_WIDTH_BYTES;
 
     const size_t mem_bytes = bytes_per_vpu_word * max_test_vpu_words;
+
     int8_t * src = malloc(bytes_per_vpu_word * max_test_vpu_words + 4);
     int8_t * dst = malloc(bytes_per_vpu_word * max_test_vpu_words + 4);
     
@@ -45,14 +49,17 @@ void test_vpu_memcpy_pseudo_random(){
     if ((int)dst&3) dst += (3 - ((int)dst&3));
 
     int seed = 69;
-    for(size_t src_offset = 0; src_offset < mem_bytes; src_offset += 4){
+    for(size_t src_offset = 0; src_offset < mem_bytes; src_offset += pointer_inc){
 
-        for(size_t dst_offset = src_offset; dst_offset < mem_bytes; dst_offset += 4){
+        for(size_t dst_offset = src_offset; dst_offset < mem_bytes; dst_offset += pointer_inc){
 
             size_t max_cpy_bytes = mem_bytes - dst_offset;
 
-            for(size_t cpy_bytes = 4; cpy_bytes < max_cpy_bytes; cpy_bytes += 4){
-                memset(dst, 0x0, mem_bytes);
+            for(size_t cpy_bytes = 4; cpy_bytes < max_cpy_bytes; cpy_bytes += cpy_bytes_inc){
+
+                int8_t dst_init = (int8_t)pseudo_rand(&seed);
+
+                memset(dst, dst_init, mem_bytes);
 
                 memset(src, 0xff, mem_bytes);
 
@@ -62,11 +69,18 @@ void test_vpu_memcpy_pseudo_random(){
                 vpu_memcpy(dst + dst_offset, src + src_offset, cpy_bytes);
 
                 TEST_ASSERT_EQUAL_INT8_ARRAY(dst + dst_offset, src + src_offset, cpy_bytes);
+                if(dst_offset)
+                    TEST_ASSERT_EACH_EQUAL_INT8(dst_init, dst, dst_offset);
+                if (mem_bytes - dst_offset - cpy_bytes)
+                    TEST_ASSERT_EACH_EQUAL_INT8(dst_init, dst + dst_offset + cpy_bytes, mem_bytes - dst_offset - cpy_bytes);
             }
         }
     }
 }
 
+void test_vpu_memcpy_pseudo_random(){
+    impl_vpu_memcpy_pseudo_random(4, 4, 20);
+}
 
 void test_vpu_memcpy()
 {
