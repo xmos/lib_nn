fe133f8b9be0906723a0bd2056b1ba0b97cbff8a
Keith Moulton <keithm@xmos.com>
2021-03-11 13:38:19 -0500

    fixed some unwanted changes

--- test/benchmark/python/process_trace.py
+++ test/benchmark/python/process_trace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # Copyright 2020-2021 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
-# This Software is subject to the terms of the XMOS Public Licence: Version 1
 
 import argparse
 import re
