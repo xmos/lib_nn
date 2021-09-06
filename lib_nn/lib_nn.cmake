5e82e4852e23dd4bf13cc78521de59c2ddecfe55
Deepak Panickal <deepakpanickal@xmos.com>
2021-09-06 15:39:44 +0100

    Cleanup cmake for X86 build

--- lib_nn/lib_nn.cmake
+++ lib_nn/lib_nn.cmake
@@ -1,44 +1,34 @@
-## Source files
-file( GLOB_RECURSE    LIB_NN_C_SOURCES       "src/*.c" )
-file( GLOB_RECURSE    LIB_NN_CPP_SOURCES     "src/*.cpp" )
-file( GLOB_RECURSE    LIB_NN_ASM_SOURCES     "src/*.S"   )
+set(LIB_NN_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/api")
 
-# file( GLOB_RECURSE    LIB_NN_C_SOURCES_REF "src/arch/ref/*.c" )
-message(STATUS "lib_nn.cmake -----------------------------------------------------------------------------------")
-## Compile flags for all platforms
+# Source files
+file(GLOB_RECURSE LIB_NN_C_SOURCES "${CMAKE_CURRENT_LIST_DIR}/src/*.c")
+file(GLOB_RECURSE LIB_NN_CPP_SOURCES "${CMAKE_CURRENT_LIST_DIR}/src/*.cpp")
+file(GLOB_RECURSE LIB_NN_ASM_SOURCES "${CMAKE_CURRENT_LIST_DIR}/src/*.S")
+
+# Compile flags for all platforms
 unset(LIB_NN_COMPILE_FLAGS)
-list( APPEND  LIB_NN_COMPILE_FLAGS     -Wno-unused-variable -Wno-missing-braces)
+list(APPEND LIB_NN_COMPILE_FLAGS -Wno-unused-variable -Wno-missing-braces)
 
-## Platform-specific compile flags can go here
+# Platform-specific compile flags can go here
 unset(LIB_NN_COMPILE_FLAGS_XCORE)
-list( APPEND  LIB_NN_COMPILE_FLAGS_XCORE   -Wno-xcore-fptrgroup)
-
-## set LIB_NN_INCLUDES & LIB_NN_SOURCES
-# set( LIB_NN_INCLUDES     api  )
-
-set(LIB_NN_INCLUDE_DIRS
-  "api"
-  "src"
-)
+list(APPEND LIB_NN_COMPILE_FLAGS_XCORE -Wno-xcore-fptrgroup)
 
 unset(LIB_NN_SOURCES)
-list( APPEND  LIB_NN_SOURCES   ${LIB_NN_C_SOURCES}    )
-list( APPEND  LIB_NN_SOURCES   ${LIB_NN_CPP_SOURCES}  )
+list(APPEND LIB_NN_SOURCES ${LIB_NN_C_SOURCES})
+list(APPEND LIB_NN_SOURCES ${LIB_NN_CPP_SOURCES})
 
 unset(LIB_NN_SOURCES_XCORE)
-list( APPEND  LIB_NN_SOURCES_XCORE   ${LIB_NN_ASM_SOURCES} )
+list(APPEND LIB_NN_SOURCES_XCORE ${LIB_NN_ASM_SOURCES})
 
-# unset(LIB_NN_SOURCES_Linux) 
-# list( APPEND  LIB_NN_SOURCES_Linux   ${LIB_NN_C_SOURCES_REF} )
+# Combine platform-agnostic and platform-specific variables..
+list(APPEND LIB_NN_COMPILE_FLAGS ${LIB_NN_COMPILE_FLAGS_${CMAKE_SYSTEM_NAME}})
 
-## Combine platform-agnostic and platform-specific variables..
-list( APPEND  LIB_NN_COMPILE_FLAGS    ${LIB_NN_COMPILE_FLAGS_${CMAKE_SYSTEM_NAME}} )
+list(APPEND LIB_NN_SOURCES ${LIB_NN_SOURCES_${CMAKE_SYSTEM_NAME}})
 
-list( APPEND  LIB_NN_SOURCES  ${LIB_NN_SOURCES_${CMAKE_SYSTEM_NAME}}        )
-
-## cmake doesn't recognize .S files as assembly by default
-set_source_files_properties( ${LIB_NN_ASM_SOURCES} PROPERTIES LANGUAGE ASM )
+# cmake doesn't recognize .S files as assembly by default
+set_source_files_properties(${LIB_NN_ASM_SOURCES} PROPERTIES LANGUAGE ASM)
 
 foreach(COMPILE_FLAG ${LIB_NN_COMPILE_FLAGS})
-  set_source_files_properties( ${LIB_NN_SOURCES} PROPERTIES COMPILE_FLAGS ${COMPILE_FLAG}) 
+  set_source_files_properties(${LIB_NN_SOURCES} PROPERTIES COMPILE_FLAGS
+                                                           ${COMPILE_FLAG})
 endforeach()
--- test/etc/Unity/Unity.cmake
+++ test/etc/Unity/Unity.cmake
@@ -1,33 +1,33 @@
 
+function(add_unity_to_target target_name)
 
-function( add_unity_to_target  target_name )
-
-  set( UNITY_PATH ${CMAKE_BINARY_DIR}/../../deps/unity )
+  set(UNITY_PATH ${CMAKE_CURRENT_LIST_DIR}/../deps/unity)
 
   unset(UNITY_SOURCES)
-  list( APPEND UNITY_SOURCES
-      "${UNITY_PATH}/src/unity.c"
+  list(APPEND UNITY_SOURCES "${UNITY_PATH}/src/unity.c")
+
+  target_sources(${target_name} PRIVATE ${UNITY_SOURCES})
+
+  target_include_directories(${target_name} PRIVATE ${UNITY_PATH}/src)
+
+  message(
+    STATUS
+      "unity.cmake -----------------------------------------------------------------------------------"
   )
 
-  target_sources( ${target_name} PRIVATE ${UNITY_SOURCES} )
-
-  target_include_directories( ${target_name} PRIVATE 
-      ${UNITY_PATH}/src
-  )
-
-  message(STATUS "unity.cmake -----------------------------------------------------------------------------------")
-
-  # If "CONFIG" is given as an optional argument, tell Unity to include "unity_config.h"
-  # (user should make sure unity_config.h is in the include path)
-  if (CONFIG IN_LIST ARGN)
-    target_compile_definitions( ${target_name} PRIVATE "UNITY_INCLUDE_CONFIG_H")
+  # If "CONFIG" is given as an optional argument, tell Unity to include
+  # "unity_config.h" (user should make sure unity_config.h is in the include
+  # path)
+  if(CONFIG IN_LIST ARGN)
+    target_compile_definitions(${target_name} PRIVATE "UNITY_INCLUDE_CONFIG_H")
   endif()
-  
-  if( XCORE )
-    list( APPEND  UNITY_FLAGS  -Wno-xcore-fptrgroup )
+
+  if(XCORE)
+    list(APPEND UNITY_FLAGS -Wno-xcore-fptrgroup)
   endif()
 
   string(REPLACE ";" " " UNITY_FLAGS "${UNITY_FLAGS}")
-  set_source_files_properties( ${UNITY_SOURCES} PROPERTIES  COMPILE_FLAGS  "${UNITY_FLAGS}" )
+  set_source_files_properties(${UNITY_SOURCES} PROPERTIES COMPILE_FLAGS
+                                                          "${UNITY_FLAGS}")
 
 endfunction()
