90171bdc98d92ec14e3150ceb09f3e445075b563
Deepak Panickal <deepakpanickal@xmos.com>
2022-12-20 15:53:12 +0000

    Update version and include dir

--- lib_nn/api/version.h
+++ lib_nn/api/version.h
@@ -4,9 +4,9 @@
 
 namespace lib_nn {
 
-unsigned major_version = 0;
-unsigned minor_version = 1;
-unsigned patch_version = 0;
+static const unsigned major_version = 0;
+static const unsigned minor_version = 1;
+static const unsigned patch_version = 0;
 
 }  // namespace lib_nn
 
