77290251ca1cfbb78352dbc1b5a08a1fee402ca7
panickal-xmos <deepakpanickal@xmos.com>
2023-02-09 19:28:58 +0000

    Fix for windows

--- lib_nn/src/cpp/MemCpyFn.cpp
+++ lib_nn/src/cpp/MemCpyFn.cpp
@@ -200,7 +200,7 @@ ImToColValid::Params::Params(const ImageGeometry &X, const WindowGeometry &K,
   int bytes_actually_copied =
       (input_channel_groups + 1) * XS3_VPU_VREG_WIDTH_BYTES;
   T_rewind = bytes_actually_copied - bytes_per_copy_per_channel - 32;
-  uint bitsleft = bytes_per_copy_per_channel & (XS3_VPU_VREG_WIDTH_BYTES - 1);
+  uint32_t bitsleft = bytes_per_copy_per_channel & (XS3_VPU_VREG_WIDTH_BYTES - 1);
   if (bitsleft != 0) {
     T_vstrpv_mask = (1ULL << bitsleft) -1;
   } else {
