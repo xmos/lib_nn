a28bb56dc63e2704dae445d2bdebc5ded223ee70
Henk Muller <henk@xmos.com>
2024-01-31 17:46:47 +0000

    Missing .h files

---
+++ lib_nn/api/quantize_int16_transform.h
@@ -0,0 +1,32 @@
+#ifndef _quantize_int16_transform_h_
+#define _quantize_int16_transform_h_
+
+#include <stdint.h>
+
+/**
+ * Function that performs the compile time transformation of an int16 addition
+ * between two tensors.
+ * 
+ * this function should be
+ * called at compile-time, and at run-time the output of this function shall be passed
+ * as the second input tensor of ``quantize_int16_tensor``
+ * 
+ * @param output            Output of the function; a blob of
+ *                          ``QUANTIZE_INT16_TENSOR_BYTES()`` bytes.
+ *                          Must be word-aligned.
+ *
+ * @param input_scaler      Quantisation scaler for the input
+ *
+ * @returns 1 on success, 0 on fail (fallback required)
+ */
+int quantize_int16_tensor_blob(void *output,
+                                 float input_scaler);
+
+/**
+ * Macro that calculates the number of int16_t that should be allocated to
+ * store the output of ``quantize_int16_tensor_blob()``
+ */
+#define QUANTIZE_INT16_TENSOR_BYTES()  (1 * sizeof(float))
+
+
+#endif
