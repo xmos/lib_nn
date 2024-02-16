fae4ed6b07367898924e32f710cfd656c5517b2a
Michael Poluektov <michael.poluektov@gmail.com>
2024-02-16 17:39:58 +0000

    add old pad back

--- lib_nn/api/nn_layers.h
+++ lib_nn/api/nn_layers.h
@@ -110,6 +110,50 @@ typedef struct padding_sizes_t {
 } padding_sizes_t;
 
 /**
+ * @brief Execute @oper{pad_prepare} function.
+ *
+ * `plan` points to the output vector @tensor{y} with length @math{N}.
+ *
+ * `p` struct describing the padding to be applied to the input tensor.
+ *
+ * `x` parameters describing the input tensor to be padded.
+ *
+ * `bytes_per_pixel` the bytes per pixel for tensor x.
+ *
+ * @param plan             [out]  The output vector @tensor{y}
+ * @param p                [in]   The input vector @tensor{x}
+ * @param x                [in]   Look-up table @tensor{T}
+ * @param bytes_per_pixel  [in]   Length @math{N} of input and output vectors
+ */
+C_API void pad_prepare(nn_pad_plan_t *plan, const padding_sizes_t *p,
+                       const nn_image_params_t *x,
+                       const unsigned bytes_per_pixel);
+
+/**
+ * @brief Execute @oper{pad_run} job.
+ *
+ * See @oper_ref{pad_run} for more details about the @oper{requantize_16_to_8}
+ * operator.
+ *
+ * `Y` points to the output vector @tensor{y}.
+ *
+ * `X` points to the input vector @tensor{x}.
+ *
+ * `plan` points to the (initialized) plan.
+ *
+ * @requires_word_alignment{Y,X}
+ *
+ * @param y   [out]    The output vector @tensor{y}
+ * @param x   [in]     The input vector @tensor{x}
+ * @param plan [in]    The prameters describing how to pad.
+ */
+void pad_run(char *y, char *x, const nn_pad_plan_t *p, uint32_t pad_value);
+
+void pad_ref(char *y, char *x, const padding_sizes_t *p,
+             const nn_image_params_t *xp, const unsigned bytes_per_pixel,
+             uint32_t pad_value);
+
+/**
  * Func to calculate n_3
  */
 void pad_3_to_4_prepare(uint32_t *n_3, const unsigned height,
