750beafcc80a8d8c6071e27075d6dade8e441a5a
Henk Muller <henk@xmos.com>
2024-03-28 20:00:19 +0000

    Fies to ref implementation and avoid assembly from being compiled in ref code

--- lib_nn/src/c/vpu_memmove_word_aligned.c
+++ lib_nn/src/c/vpu_memmove_word_aligned.c
@@ -1,8 +1,8 @@
-#include <stdlib.h>
+#include <string.h>
 #include "vpu_memmove_word_aligned.h"
 
 #ifdef NN_USE_REF
-void vpu_memmove_word_aligned(void * dst, const void * src, int byte_count) {
+void vpu_memmove_word_aligned(void * dst, const void * src, unsigned byte_count) {
     memmove(dst, src, byte_count);
 }
 #endif
