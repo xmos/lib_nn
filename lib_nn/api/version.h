85701e9422e75acf6bba4a616041e239078bafac
panickal-xmos <deepakpanickal@xmos.com>
2024-10-24 09:08:19 +0100

    Update version

--- lib_nn/api/version.h
+++ lib_nn/api/version.h
@@ -1,11 +1,11 @@
-// Copyright (c) 2020, XMOS Ltd, All rights reserved
+// Copyright (c) 2024, XMOS Ltd, All rights reserved
 #ifndef LIB_NN_VERSION_H_
 #define LIB_NN_VERSION_H_
 
 namespace lib_nn {
 
 static const unsigned major_version = 0;
-static const unsigned minor_version = 3;
+static const unsigned minor_version = 4;
 static const unsigned patch_version = 0;
 
 }  // namespace lib_nn
