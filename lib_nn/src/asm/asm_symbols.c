4f85126c504065d813f3be6c4699d95adf320262
andrewstanfordjason <andrew@xmos.com>
2025-02-27 15:25:44 +0000

    added xs3 guards to inline asm

--- lib_nn/src/asm/asm_symbols.c
+++ lib_nn/src/asm/asm_symbols.c
@@ -2,7 +2,7 @@
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
 #if __xcore__
-
+#ifdef __XS3A__
 asm(".set nn_bso_block_t_sizeof,  (7*8*4); .global nn_bso_block_t_sizeof");
-
+#endif
 #endif
