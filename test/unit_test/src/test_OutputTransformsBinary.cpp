c3a65fe19015d23d91862f9d823a54b2b0034fb0
xalbertoisorna <albertoisorna@xmos.com>
2026-09-09 14:54:37 +0100

    Refactor output_transform_fn_binary_asm and update tests to remove implementation checks for VX4

--- test/unit_test/src/test_OutputTransformsBinary.cpp
+++ test/unit_test/src/test_OutputTransformsBinary.cpp
@@ -52,10 +52,6 @@ static int8_t *_test_ot_binnary_helper(
 
 TEST(group_output_transforms_binary, Test_otfn_binary_negative_sum)
 {
-#if defined(__VX4A__) || defined(__VX4B__)
-    TEST_IGNORE_MESSAGE("otfn_binary is not implemented on VX4");
-#endif
-
     // a + t = -1
     const int16_t accumulator = 3;
     const int16_t threshold = -4;
@@ -69,10 +65,6 @@ TEST(group_output_transforms_binary, Test_otfn_binary_negative_sum)
 
 TEST(group_output_transforms_binary, Test_otfn_binary_positive_sum)
 {
-#if defined(__VX4A__) || defined(__VX4B__)
-    TEST_IGNORE_MESSAGE("otfn_binary is not implemented on VX4");
-#endif
-
     // a + t = 5
     const int16_t accumulator = 2;
     const int16_t threshold = 3;
@@ -86,10 +78,6 @@ TEST(group_output_transforms_binary, Test_otfn_binary_positive_sum)
 
 TEST(group_output_transforms_binary, Test_otfn_binary_random_values)
 {
-#if defined(__VX4A__) || defined(__VX4B__)
-    TEST_IGNORE_MESSAGE("otfn_binary is not implemented on VX4");
-#endif
-
     int seed = 0x6D2B79F5;
     int16_t th[VPU_INT16_EPV];
     VPURingBuffer acc{};
