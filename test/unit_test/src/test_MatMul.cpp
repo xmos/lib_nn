4db31bc858654137ab93ad4057abc69f41b38c1d
uvvpavel <paveluvarov@xmos.com>
2026-09-10 13:11:22 +0100

    vx4 mamtmuls direct int16 (dw)

--- test/unit_test/src/test_MatMul.cpp
+++ test/unit_test/src/test_MatMul.cpp
@@ -134,17 +134,21 @@ TEST(group_mat_mul, Test_DepthwiseInt8) {
 }
 
 TEST(group_mat_mul, Test_DepthwiseInt16) {
-#if defined(__VX4B__)
-  TEST_IGNORE_MESSAGE("mat_mul_dw_direct_int16 assembly faults on VX4");
-#else
   MatMulDirectFn_DW dir(img_i16, win);
   mat_mul_dw_direct_params_t prm = dir.getParams();
+
+#if defined(__VX4B__)
+  // VX4B VLMACC drops the product's least-significant bit.
+  constexpr int16_t weight_value = 2;
+#else
+  constexpr int16_t weight_value = 1;
+#endif
+
   std::fill_n(inp_i16, chn_n, 1);
-  std::fill_n(wgt_i16, dw_wgt_n, 1);
+  std::fill_n(wgt_i16, dw_wgt_n, weight_value);
 
   mat_mul_dw_direct_int16(&prm, &A, inp_i16, ocg, wgt_i16);
-  assert_accumulator_value(A, 1);
-#endif
+  assert_accumulator_value(A, weight_value);
 }
 
 }  // extern "C"
