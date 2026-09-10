4a2f0aa1a9d7bfcd0d909e84d2ce48b29037db99
xalbertoisorna <albertoisorna@xmos.com>
2026-09-10 13:58:39 +0100

    adding output_transform_fn_int_clamped

--- test/unit_test/src/main.c
+++ test/unit_test/src/main.c
@@ -23,6 +23,7 @@ int main(int argc, const char* argv[]) {
   RUN_TEST_GROUP(group_multiply_int16);
   RUN_TEST_GROUP(group_output_transform_fn_int16);
   RUN_TEST_GROUP(group_output_transforms_binary);
+  RUN_TEST_GROUP(group_output_transforms_clamped);
   RUN_TEST_GROUP(group_pad);
   RUN_TEST_GROUP(group_quantize_int16);
   RUN_TEST_GROUP(group_quadratic_interpolation);
